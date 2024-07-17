724942120cc12c81e3da3a2fa3be07fb954dd23b
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2024-07-17 16:07:52 -0700

    creating code to subset biolink

--- src/scripts/transform.py
+++ src/scripts/transform.py
@@ -1,12 +1,32 @@
-from linkml_map.session import Session
-import graphviz
-from linkml_runtime.utils.schemaview import SchemaView
-from pprint import pprint
-from linkml_runtime.dumpers import yaml_dumper
+from typing import List
 
+from linkml_map.session import Session
+from linkml_model.meta import ClassDefinition
+import yaml
+from linkml.utils.schema_builder import SchemaBuilder
+from linkml_runtime.utils.schemaview import SchemaView
+import requests
+import tempfile
+from pprint import pprint
+
+# URL of the schema
 # Load the schema
-schema_path = "src/linkml_map_example/schema/linkml_map_example.yaml"
-sv = SchemaView(schema_path)
+schema_url = "https://raw.githubusercontent.com/biolink/biolink-model/master/biolink-model.yaml"
+
+# Fetch the schema from the URL
+response = requests.get(schema_url)
+response.raise_for_status()  # Raise an error for bad status codes
+
+# Load the schema content into SchemaView
+schema_content = response.text
+
+with tempfile.NamedTemporaryFile(delete=False, suffix='.yaml') as temp_file:
+    temp_file.write(schema_content.encode())
+    temp_file_path = temp_file.name
+
+sv = SchemaView(temp_file_path)
+# Load the schema
+sv = SchemaView(schema_content)
 
 # Initialize Session and SchemaBuilder
 session = Session()
@@ -14,36 +34,70 @@ session = Session()
 # Set the source schema in the session
 session.set_source_schema(sv)
 
+SUBSET_CLASSES = ["gene",
+                  "disease",
+                  "case to phenotypic feature association",
+                  "gene to disease association",
+                  "gene to phenotypic feature association",
+                  "case",
+                  "phenotypic feature",
+                  ]
+
+
+# Function to get Biolink class definitions
+def get_biolink_classes_definitions(sv, subset_classes) -> List[ClassDefinition]:
+    # Example implementation to fetch class definitions
+    # This should be replaced with the actual implementation
+    class_definitions = []
+    for class_name in subset_classes:
+        class_def = sv.induced_class(class_name)
+        class_definitions.append(class_def)
+    return class_definitions
+
+
+# Function to build subset schema
+def build_subset_schema(class_defs: List[ClassDefinition]) -> SchemaBuilder:
+    sb = SchemaBuilder(name="SubsetSchema", id="https://example.org/SubsetSchema")
+    for class_def in class_defs:
+        for slot_def in class_def.slots:
+            sb.add_slot(slot_def)
+        sb.add_class(class_def)
+    sb.add_defaults()
+    return sb
+
+
+class_definitions = get_biolink_classes_definitions(sv, SUBSET_CLASSES)
+built_schema = build_subset_schema(class_definitions)
+built_schema.prefixes = sv.schema.prefixes
+output_file_path = "subset_schema.yaml"
+with open(output_file_path, "w") as file:
+    yaml.dump(built_schema.as_dict(), file, sort_keys=False)
+
 
 # Transformer specification (in YAML)
 session.set_object_transformer("""
 class_derivations:
-    Person:
+    GeneToDiseaseAssociation:
+        populated_from: GeneToDiseaseAssociation
+    GeneToPhenotypicFeatureAssociation:
+        populated_from: GeneToPhenotypicFeatureAssociation
+    Disease:
+        populated_from: Disease
+    PhenotypicFeature:
+        populated_from: PhenotypicFeature
+    Case:
         populated_from: Person
-        slot_derivations:
-            given_name:
-                populated_from: first_name
-            family_name:
-                populated_from: last_name
-            name:
-                expr: "{first_name} + ' ' + {last_name}"    
-            age_in_months:
-                expr: age_in_years * 12
-                range: integer
+    Gene:
+        populated_from: Gene
+    CaseToPhenotypicFeatureAssociation:
+        populated_from: CaseToPhenotypicFeatureAssociation
 """)
 
-obj = {
-        "first_name": "Jane",
-        "last_name": "Doe",
-        "age_in_years": 25,
-}
 
-# print(session.transform(obj))
-print(yaml_dumper.dumps(session.target_schema))
-
-dot = session.graphviz()
-if isinstance(dot, graphviz.Digraph):
-    dot.render('session_graph', format='png', cleanup=True)
-    dot.view()
-else:
-    print("graphviz() did not return a Digraph object.")
+#
+# dot = session.graphviz()
+# if isinstance(dot, graphviz.Digraph):
+#     dot.render('session_graph', format='png', cleanup=True)
+#     dot.view()
+# else:
+#     print("graphviz() did not return a Digraph object.")
